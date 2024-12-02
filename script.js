6216e7538d5642c08d04b4252644252d54785908
Prathmesh1604 <prathmeshbagalkote10@gmail.com>
2024-12-02 16:29:02 +0530

    message

---
+++ script.js
@@ -0,0 +1,41 @@
+function navAnimation(){
+    var nav=document.querySelector("nav");
+nav.addEventListener("mouseenter",function(e){
+    var tl=gsap.timeline()
+    tl.to("#nav-bottom",{
+        height:"21vh"
+    })
+    tl.to(".nav-part2 h5", {
+        display: "block",
+        duration: 0.1
+
+    })
+    tl.from(".nav-part2 h5 span", {
+        y:22,
+        stagger:{
+            amount:0.5
+        }
+       
+    })
+
+})
+nav.addEventListener("mouseleave", function () {
+    let tl = gsap.timeline()
+    tl.to(".nav-part2 h5 span", {
+        y: 22,
+        stagger: {
+            amount: 0.2
+        }
+    })
+    tl.to(".nav-part2 h5", {
+        display: "none",
+        duration: 0.1
+    })
+    tl.to("#nav-bottom", {
+        height: 0,
+        duration: 0.2
+    })
+})
+}
+navAnimation()
+
